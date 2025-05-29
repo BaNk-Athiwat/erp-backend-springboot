30277e845c25df1c4d1764eb86cdfe14dda83928
BaNk-Athiwat <athiwat.sitthinanwatthana@gmail.com>
2025-05-29 22:58:48 +0700

    feat: enhance user management with admin functionalities and ordered user retrieval

--- src/main/java/com/app/erp_backend_springboot/controllers/UserController.java
+++ src/main/java/com/app/erp_backend_springboot/controllers/UserController.java
@@ -39,7 +39,7 @@ public class UserController {
 
     @GetMapping
     public List<UserEntity> getAllUsers() {
-        return userRepository.findAll();
+        return userRepository.findAllByOrderByName();
     }
     
     @PostMapping
@@ -145,14 +145,90 @@ public class UserController {
                 throw new IllegalArgumentException("User not found");
             }
 
-            record UserResponse(Long id, String username, String email) {}
+            record UserResponse(Long id, String name, String username, String email) {}
 
-            return new UserResponse(user.getId(), user.getUsername(), user.getEmail());
+            return new UserResponse(user.getId(), user.getName(), user.getUsername(), user.getEmail());
 
         } catch (Exception e) {
             // TODO: handle exception
             throw new IllegalArgumentException("Authorization error: " + e.getMessage());
         }
     }
+
+    public Long getUserIdFromToken(String bearerToken) {
+        String token = bearerToken.replace("Bearer ", "");
+
+        if (token.trim().isEmpty()) {
+            throw new IllegalArgumentException("Token is empty");
+        }
+
+        return Long.valueOf(
+            JWT.require(getAlgorithm())
+            .build()
+            .verify(token)
+            .getSubject()
+        );
+    }
+
+    @PutMapping("/admin-edit-profile/{id}")
+    public UserEntity adminEditProfilEntity(
+        @RequestHeader("Authorization") String token,
+        @RequestBody UserEntity user,
+        @PathVariable Long id
+    ) {
+        try {
+            // Long userId = getUserIdFromToken(token);
+            UserEntity userToUpdate = userRepository.findById(id).orElse(null);
+    
+            if (userToUpdate == null) {
+                throw new IllegalArgumentException("User not found");
+            }
+    
+            userToUpdate.setName(user.getName());
+            userToUpdate.setUsername(user.getUsername());
+            userToUpdate.setEmail(user.getEmail());
+    
+            if (user.getPassword() != null && !user.getPassword().isEmpty()) {
+                userToUpdate.setPassword(user.getPassword());
+            }
+    
+            userRepository.save(userToUpdate);
+    
+            return userToUpdate;
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            throw new IllegalArgumentException("Update user error: " + e.getMessage());
+        }
+    }
+    
+    @PostMapping("/admin-create")
+    public UserEntity adminCreateEntity(@RequestBody UserEntity user) {
+        try {
+            userRepository.save(user);
+            return user;
+        } catch (Exception e) {
+            // TODO: handle exception
+            throw new IllegalArgumentException("Create user error: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/admin-delete/{id}")
+    public void adminDelete(@PathVariable Long id) {
+        try {
+            UserEntity userToDelete = userRepository.findById(id).orElse(null);
+
+            if (userToDelete == null) {
+                throw new IllegalArgumentException("User not found");
+            }
+
+            userRepository.deleteById(id);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            throw new IllegalArgumentException("Delete user error: " + e.getMessage());
+        }
+    }
+    
     
 }
--- src/main/java/com/app/erp_backend_springboot/repository/UserRepository.java
+++ src/main/java/com/app/erp_backend_springboot/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.app.erp_backend_springboot.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.erp_backend_springboot.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    List<UserEntity> findAllByOrderByName();
     UserEntity findByUsernameAndEmail(String username, String email);
     UserEntity findByUsernameAndPassword(String username, String password);
 
